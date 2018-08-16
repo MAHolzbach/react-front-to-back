1b9f219d74ce29411644a94ef5b44fdb3343e053
Mike Holzbach <mikeholzbach@a-w3595htd6.carfax.cfx>
2018-08-16 08:34:13 -0400

    Review

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -2,15 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+  onShowClick = e => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
   render() {
     const { name, email, phone } = this.props;
+    const { showContactInfo } = this.state;
     return (
       <div className="card">
-        <h4 className="card-header-title">{name}</h4>
-        <div className="card-content">
-          <p className="panel-block">Email: {email}</p>
-          <p className="panel-block">Phone: {phone}</p>
-        </div>
+        <h4 className="card-header-title">
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        {showContactInfo ? (
+          <div className="card-content">
+            <p className="panel-block">Email: {email}</p>
+            <p className="panel-block">Phone: {phone}</p>
+          </div>
+        ) : null}
       </div>
     );
   }
